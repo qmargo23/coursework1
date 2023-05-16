f3c5869ec2aac0e0f2dd643948f18716535ce837
qmargo23 <elen.asplund@gmail.com>
2023-05-16 22:40:24 +0300

    курсовая_выполнение_работы_001

--- Employee.java
+++ Employee.java
@@ -4,6 +4,37 @@ public class Employee {
     private float salary;
     int id = Main.cnt;
 
+    public Employee(String FIO, int department, float salary) {
+        this.FIO = FIO;
+        this.department = department;
+        this.salary = salary;
+        Main.cnt++;
 
+    }
+
+    public String getFIO() {
+        return FIO;
+    }
+    public int getDepartment() {
+        return department;
+    }
+    public float getSalary() {
+        return salary;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return id +": сотрудник - " + FIO + ", отдел - " + department + ", зарплата - " + salary +".";
+    }
 
 }
